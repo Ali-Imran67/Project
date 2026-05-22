54ca4ef1f855f1caadbfd9cc34e69086a6efcbb4
Asjad <toman9037@gmail.com>
2026-05-22 16:38:53 +0500

    added Paddle header file

---
+++ include/paddle.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "shape.hpp"
+
+class Paddle : public shape {
+
+private:
+    float width;
+    float height;
+    sf::RectangleShape rect;   // SFML drawable rectangle
+
+public:
+    // Constructor
+    Paddle(Vector2f initialPos, Vector2f initialSpeed, float width, float height);
+
+    // Override pure virtual draw() from shape
+    void draw(RenderWindow& window) override;
+
+    // Override update() to add boundary clamping
+    void update(float deltaTime) override;
+
+    // Move paddle up/down manually
+    void moveUp(float deltaTime);
+    void moveDown(float deltaTime, float windowHeight);
+
+    // Getters for collision detection 
+    float getWidth()  const;
+    float getHeight() const;
+
+    // Returns the SFML rect (used for collision in game loop)
+    sf::FloatRect getBounds() const;
+};
